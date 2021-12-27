881b6578ce63fdac43428bf29c1d7cc976af96be
dada513 <dada513@protonmail.com>
2021-12-27 18:21:04 +0100

    Proper versioning in makedeb.sh

--- packages/debian/makedeb.sh
+++ packages/debian/makedeb.sh
@@ -4,4 +4,7 @@ cd builddir
 cmake -DLauncher_LAYOUT=lin-system -DCMAKE_INSTALL_PREFIX=../polymc/usr ../../../
 make -j$(nproc) install
 cd ..
+VER=$(git describe --tags | sed 's/-.*//')
+sed -i "2s/.*/Version: $VER/" polymc/DEBIAN/control
 dpkg-deb --build polymc
+sed -i "2s/.*/Version: Set to latest git tag by makedeb.sh at build time/" polymc/DEBIAN/control
