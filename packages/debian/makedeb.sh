e99d7ca4fed6285f1f04749fe887ba95e38c12e3
dada513 <dada513@protonmail.com>
2021-12-28 16:40:21 +0100

    adjust makedeb.sh to use template file

--- packages/debian/makedeb.sh
+++ packages/debian/makedeb.sh
@@ -4,7 +4,7 @@ cd builddir
 cmake -DLauncher_LAYOUT=lin-system -DCMAKE_INSTALL_PREFIX=../polymc/usr ../../../
 make -j$(nproc) install
 cd ..
-VER=$(git describe --tags | sed 's/-.*//')
-sed -i "2s/.*/Version: $VER/" polymc/DEBIAN/control
+VERSION_PLACEHOLDER=$(git describe --tags | sed 's/-.*//')
+cp polymc/DEBIAN/control.template polymc/DEBIAN/control
+sed -i "2s/.*/Version: $VERSION_PLACEHOLDER/" polymc/DEBIAN/control
 dpkg-deb --build polymc
-sed -i "2s/.*/Version: Set to latest git tag by makedeb.sh at build time/" polymc/DEBIAN/control
