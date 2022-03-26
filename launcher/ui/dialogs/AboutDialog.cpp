f5f022b23840b1b1ab9e174e162100a63b5d5b05
Sefa Eyeoglu <contact@scrumplex.net>
2022-03-26 23:16:08 +0100

    fix(credits): wrap UTF-8 text with QString

--- launcher/ui/dialogs/AboutDialog.cpp
+++ launcher/ui/dialogs/AboutDialog.cpp
@@ -65,7 +65,7 @@ QString getCreditsHtml()
     // TODO: possibly retrieve from git history at build time?
     stream << "<h3>" << QObject::tr("MultiMC Developers", "About Credits") << "</h3>\n";
     stream << "<p>Andrew Okin &lt;<a href='mailto:forkk@forkk.net'>forkk@forkk.net</a>&gt;</p>\n";
-    stream << "<p>Petr Mrázek &lt;<a href='mailto:peterix@gmail.com'>peterix@gmail.com</a>&gt;</p>\n";
+    stream << QString("<p>Petr Mrázek &lt;<a href='mailto:peterix@gmail.com'>peterix@gmail.com</a>&gt;</p>\n");
     stream << "<p>Sky Welch &lt;<a href='mailto:multimc@bunnies.io'>multimc@bunnies.io</a>&gt;</p>\n";
     stream << "<p>Jan (02JanDal) &lt;<a href='mailto:02jandal@gmail.com'>02jandal@gmail.com</a>&gt;</p>\n";
     stream << "<p>RoboSky &lt;<a href='https://twitter.com/RoboSky_'>@RoboSky_</a>&gt;</p>\n";
