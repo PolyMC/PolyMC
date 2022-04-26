f7251df65f190a6bfe030405c03faeed171541ae
Kenneth Chew <kenneth.c0@protonmail.com>
2022-04-26 15:09:50 -0400

    Add parameter for major Qt version

--- .github/scripts/macos_fix_qt_structure.sh
+++ .github/scripts/macos_fix_qt_structure.sh
@@ -3,8 +3,14 @@
 # A script that fixes macOS Qt framework weirdness.
 # See https://stackoverflow.com/questions/27952111/unable-to-sign-app-bundle-using-qt-frameworks-on-os-x-10-10/28097138#28097138
 
+# Usage: ./macos_fix_qt_structure.sh path_to_app [major_qt_version]
+
 # retrieve bundle name from first parameter
 BUNDLE_NAME=$1
+
+# retrieve Qt major version from second parameter (5 by default)
+QT_MAJOR_VERSION=$2
+QT_MAJOR_VERSION="${QT_MAJOR_VERSION:-5}"
 
 # if can't change directory, then quit immediately
 cd "${BUNDLE_NAME}/Contents/Frameworks/" || return 1
@@ -14,11 +20,11 @@ for CURRENT_FRAMEWORK in Qt*; do
 
     CURRENT_FRAMEWORK_NAME="${CURRENT_FRAMEWORK%.*}"
 
-    mkdir -p "${CURRENT_FRAMEWORK}/Versions/5"
+    mkdir -p "${CURRENT_FRAMEWORK}/Versions/${QT_MAJOR_VERSION}"
 
-    mv "${CURRENT_FRAMEWORK}/Resources" "${CURRENT_FRAMEWORK}/Versions/5/Resources"
+    mv "${CURRENT_FRAMEWORK}/Resources" "${CURRENT_FRAMEWORK}/Versions/${QT_MAJOR_VERSION}/Resources"
 
-    ln -nfs "5"                                          "${CURRENT_FRAMEWORK}/Versions/Current"
+    ln -nfs "${QT_MAJOR_VERSION}"                        "${CURRENT_FRAMEWORK}/Versions/Current"
     ln -nfs "Versions/Current/${CURRENT_FRAMEWORK_NAME}" "${CURRENT_FRAMEWORK}/${CURRENT_FRAMEWORK_NAME}"
     ln -nfs "Versions/Current/Resources"                 "${CURRENT_FRAMEWORK}/Resources"
 done
