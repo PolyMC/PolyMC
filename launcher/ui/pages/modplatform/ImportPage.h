13f5a3ce4cf47b110afdd37a323663069419c397
Ezekiel Smith <ezekielsmith@protonmail.com>
2022-09-02 21:38:18 +1000

    Fix naming scheme for importing and creating modpacks

--- launcher/ui/pages/modplatform/ImportPage.h
+++ launcher/ui/pages/modplatform/ImportPage.h
@@ -57,7 +57,7 @@ public:
     virtual ~ImportPage();
     virtual QString displayName() const override
     {
-        return tr("Import from zip");
+        return tr("Import file");
     }
     virtual QIcon icon() const override
     {
