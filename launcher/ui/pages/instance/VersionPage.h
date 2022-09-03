e0758502ca0f1cf4a11818fe9ff7081eaa989158
Ezekiel Smith <ezekielsmith@protonmail.com>
2022-09-03 12:46:48 +1000

    Remove redundant "install mods" option

--- launcher/ui/pages/instance/VersionPage.h
+++ launcher/ui/pages/instance/VersionPage.h
@@ -84,7 +84,6 @@ private slots:
     void on_actionReplace_Minecraft_jar_triggered();
     void on_actionRevert_triggered();
     void on_actionEdit_triggered();
-    void on_actionInstall_mods_triggered();
     void on_actionCustomize_triggered();
     void on_actionDownload_All_triggered();
 
