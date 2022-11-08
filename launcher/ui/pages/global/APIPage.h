b3fb52ce512dd0afdc433efcb467cc23e3855585
Lenny McLennington <lenny@sneed.church>
2022-11-08 21:32:35 +0000

    chore: add jdp_'s copyright header
    Signed-off-by: Lenny McLennington <lenny@sneed.church>

--- launcher/ui/pages/global/APIPage.h
+++ launcher/ui/pages/global/APIPage.h
@@ -4,6 +4,7 @@
  *  Copyright (C) 2022 Sefa Eyeoglu <contact@scrumplex.net>
  *  Copyright (c) 2022 Jamie Mansfield <jmansfield@cadixdev.org>
  *  Copyright (c) 2022 Lenny McLennington <lenny@sneed.church>
+ *  Copyright (c) 2022 jdp_ (https://github.com/jdpatdiscord)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
