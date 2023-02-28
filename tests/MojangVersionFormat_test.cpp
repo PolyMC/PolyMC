55bb1b55b2b84d7142b40731dc246ed6d123b7d0
sightem <sightem@national.shitposting.agency>
2023-02-27 20:00:13 -0500

    Fix MojangVersionFormat_test.cpp
    Signed-off-by: sightem <sightem@national.shitposting.agency>

--- tests/MojangVersionFormat_test.cpp
+++ tests/MojangVersionFormat_test.cpp
@@ -9,13 +9,6 @@ class MojangVersionFormatTest : public QObject
 
     static nlohmann::json readJson(const QString path)
     {
-        /*
-        QFile jsonFile(path);
-        jsonFile.open(QIODevice::ReadOnly);
-        auto data = jsonFile.readAll();
-        jsonFile.close();
-        return QJsonDocument::fromJson(data);
-        */
         std::ifstream jsonFile(path.toStdString());
         nlohmann::json doc = nlohmann::json::parse(jsonFile);
         jsonFile.close();
@@ -23,14 +16,6 @@ class MojangVersionFormatTest : public QObject
     }
     static void writeJson(const char *file, nlohmann::json doc)
     {
-        /*
-        QFile jsonFile(file);
-        jsonFile.open(QIODevice::WriteOnly | QIODevice::Text);
-        auto data = doc.toJson(QJsonDocument::Indented);
-        qDebug() << QString::fromUtf8(data);
-        jsonFile.write(data);
-        jsonFile.close();
-        */
         std::ofstream jsonFile(file);
         jsonFile << doc.dump(4);
         jsonFile.close();
@@ -45,7 +30,6 @@ slots:
         auto doc2 = MojangVersionFormat::versionFileToJson(vfile);
         writeJson("1.9-simple-passthorugh.json", doc2);
 
-        //QCOMPARE(doc.toJson(), doc2.toJson());
         QCOMPARE(doc.dump(), doc2.dump());
     }
 
@@ -55,7 +39,7 @@ slots:
         auto vfile = MojangVersionFormat::versionFileFromJson(doc, "1.9.json");
         auto doc2 = MojangVersionFormat::versionFileToJson(vfile);
         writeJson("1.9-passthorugh.json", doc2);
-        //QCOMPARE(doc.toJson(), doc2.toJson());
+
         QCOMPARE(doc.dump(), doc2.dump());
     }
 };
