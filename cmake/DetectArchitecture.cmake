717b7658ead59ac81a708b44e4015b458a70262f
crueter <crueter@eden-emu.dev>
2026-02-27 13:29:52 -0500

    Add Windows on ARM to workflows
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- cmake/DetectArchitecture.cmake
+++ cmake/DetectArchitecture.cmake
@@ -41,7 +41,7 @@ if (CMAKE_OSX_ARCHITECTURES)
 
     # hope and pray the architecture names match
     foreach(ARCH ${CMAKE_OSX_ARCHITECTURES})
-        set(ARCHITECTURE_${ARCH} 1 PARENT_SCOPE)
+        set(ARCHITECTURE_${ARCH} 1)
         add_definitions(-DARCHITECTURE_${ARCH}=1)
     endforeach()
 
