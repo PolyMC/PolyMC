b07a28142d503d21e07936542ed197ec40b95464
crueter <crueter@eden-emu.dev>
2026-02-27 13:23:37 -0500

    Fix macOS
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- cmake/DetectArchitecture.cmake
+++ cmake/DetectArchitecture.cmake
@@ -40,7 +40,7 @@ if (CMAKE_OSX_ARCHITECTURES)
     set(ARCHITECTURE "${CMAKE_OSX_ARCHITECTURES}")
 
     # hope and pray the architecture names match
-    foreach(ARCH IN ${CMAKE_OSX_ARCHITECTURES})
+    foreach(ARCH ${CMAKE_OSX_ARCHITECTURES})
         set(ARCHITECTURE_${ARCH} 1 PARENT_SCOPE)
         add_definitions(-DARCHITECTURE_${ARCH}=1)
     endforeach()
