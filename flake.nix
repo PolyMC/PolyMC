4ec19a3849c431136e8a2de9387016f3069ef82e
Norbert <63416481+noriban@users.noreply.github.com>
2026-08-24 09:28:53 +0000

    change to nixos

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs2511.url = "github:nixos/nixpkgs/nixpkgs-25.11";
+    nixpkgs2511.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     libnbtplusplus = { url = "github:PolyMC/libnbtplusplus"; flake = false; };
     tomlplusplus = { url = "github:marzer/tomlplusplus"; flake = false; };
